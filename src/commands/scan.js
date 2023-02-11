3bfd7e152898007aef839a27ba9f69a392c2adf3
JokyBzylow <JokyBzylow@users.noreply.github.com>
2023-02-11 14:57:20 +0000

    Check now with http and https

--- src/commands/scan.js
+++ src/commands/scan.js
@@ -13,18 +13,16 @@ const introEmbed = (url, detectionStatus) =>
 		.setColor(0xffffff)
 		.setTitle('Website checker')
 		.setDescription(
-            'Scanning for Threats \n' + 
-			'This message will be edited when the scanning finishes. \n' +
-            'For new urls it may take 1-4 minutes to receive a result.',
+			'Scanning for Threats \n' +
+				'This message will be edited when the scanning finishes. \n' +
+				'For new urls it may take 1-4 minutes to receive a result.',
 		)
-        .addFields(
-            { name: 'URL', value: url},
-            { name: 'Detection Status', value: `${detectionStatus}`},
-        )
-        .setFooter({
-            text: 'Powered by the ScamSniffer Detector API',
-            iconURL: 'https://cdn.discordapp.com/attachments/1040295546352574504/1073957863971635210/scamSnifferLogo.png'
-        });
+		.addFields({ name: 'URL', value: url }, { name: 'Detection Status', value: `${detectionStatus}` })
+		.setFooter({
+			text: 'Powered by the ScamSniffer Detector API',
+			iconURL:
+				'https://cdn.discordapp.com/attachments/1040295546352574504/1073957863971635210/scamSnifferLogo.png',
+		});
 
 const safeEmbed = (url) =>
 	new EmbedBuilder()
@@ -34,94 +32,69 @@ const safeEmbed = (url) =>
 			`The url: ${url} looks safe**\n` +
 				'** \n' +
 				"Although ScamSniffer did not detect a threat doesn't mean it's totally safe. \n" +
-                "Always look closely at what is requested to sign. \n" +
-				"When in doubt ask fellow gm.embers for help.",
+				'Always look closely at what is requested to sign. \n' +
+				'When in doubt ask fellow gm.embers for help.',
 		)
-        .addFields(
-            { name: 'URL', value: url},
-            { name: 'Detection Status', value: 'NO THREATS DETECTED'},
-        )
-        .setFooter({
-            text: 'Powered by the ScamSniffer Detector API',
-            iconURL: 'https://cdn.discordapp.com/attachments/1040295546352574504/1073957863971635210/scamSnifferLogo.png'
-        });
-
+		.addFields({ name: 'URL', value: url }, { name: 'Detection Status', value: 'NO THREATS DETECTED' })
+		.setFooter({
+			text: 'Powered by the ScamSniffer Detector API',
+			iconURL:
+				'https://cdn.discordapp.com/attachments/1040295546352574504/1073957863971635210/scamSnifferLogo.png',
+		});
 
 // From the Exploits array creates an array with the embed fields
 function createThreatFields(url, array) {
-    let embedFields = [
-        { name: 'URL', value: url},
-        { name: 'Detection Status', value: 'UNSAFE. DO NOT INTERACT'}
-    ];
+	let embedFields = [
+		{ name: 'URL', value: url },
+		{ name: 'Detection Status', value: 'UNSAFE. DO NOT INTERACT' },
+	];
 
-    array.forEach(function (exploit, i) {
-        embedFields.push({name: `exploit${i}`, value: exploit, inline: true});
-    });
+	array.forEach(function (exploit, i) {
+		embedFields.push({ name: `exploit${i}`, value: exploit, inline: true });
+	});
 
-    return embedFields;
+	return embedFields;
 }
 
 const threatEmbed = (url, res) =>
 	new EmbedBuilder()
 		.setColor(0xff0000)
 		.setTitle('Website checker')
-		.setDescription(
-			`**THREAT DETECTED**\n\n` +
-				`Link is not safe. **DO NOT INTERACT**`,
-		)
-        .addFields(
-            createThreatFields(url, res.data['details']['actions'])
-        )
-        .setFooter({
-            text: 'Powered by the ScamSniffer Detector API',
-            iconURL: 'https://cdn.discordapp.com/attachments/1040295546352574504/1073957863971635210/scamSnifferLogo.png'
-        });
+		.setDescription(`**THREAT DETECTED**\n\n` + `Link is not safe. **DO NOT INTERACT**`)
+		.addFields(createThreatFields(url, res.data['details']['actions']))
+		.setFooter({
+			text: 'Powered by the ScamSniffer Detector API',
+			iconURL:
+				'https://cdn.discordapp.com/attachments/1040295546352574504/1073957863971635210/scamSnifferLogo.png',
+		});
 
 async function request(interaction, config, website, secondTry = false) {
 	console.warn(config);
-	await axios(config)
+	const promise = axios(config);
+	const dataPromise = promise
 		.then((res) => {
-			console.log({ res });
 			if (res.data['status']) {
-				!secondTry
-					? interaction.reply({
-							embeds: [introEmbed(disableHttpLink(website), res.data['status'])],
-							ephemeral: false,
-					  })
-					: null;
-				console.log(res.data);
 				setTimeout(() => {
 					request(interaction, config, website, true);
 				}, 5000);
 			} else {
-				console.log(res.data);
 				if (res.data['isSafe']) {
-					secondTry
-						? interaction.editReply({
-								embeds: [safeEmbed(disableHttpLink(website))],
-								ephemeral: false,
-						  })
-						: interaction.reply({
-								embeds: [safeEmbed(disableHttpLink(website))],
-								ephemeral: false,
-						  });
+					return { type: 'editReply', embed: safeEmbed(disableHttpLink(website)), isSafe: true };
 				} else {
-					secondTry
-						? interaction.editReply({
-								content: pingRole ? safetyRole : '',
-								embeds: [threatEmbed(disableHttpLink(website), res)],
-								ephemeral: false,
-						  })
-						: interaction.reply({
-								content: pingRole ? safetyRole : '',
-								embeds: [threatEmbed(disableHttpLink(website), res)],
-								ephemeral: false,
-						  });
+					return {
+						type: 'editReply',
+						content: pingRole ? safetyRole : '',
+						embed: threatEmbed(disableHttpLink(website), res),
+						isSafe: false,
+					};
 				}
 			}
 		})
 		.catch((err) => console.log(err));
+	return dataPromise;
 }
+
+let isSafe = true;
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -130,16 +103,56 @@ module.exports = {
 		.addStringOption((option) => option.setName('website').setDescription('The website to test').setRequired(true)),
 	async execute(interaction) {
 		const website = interaction.options.getString('website');
+		const websites = [`https://${website}`, `http://${website}`];
 
-		const config = {
-			method: 'get',
-			url: `https://detector.scamsniffer.io/api/detect?link=${website}&force=0`,
-			headers: {
-				Accept: 'application/json',
-				'X-API-KEY': process.env.SCAMSNIFFER_API_KEY,
-			},
-		};
+		await interaction.reply({
+			embeds: [introEmbed(disableHttpLink(website), 'CREATED')],
+			ephemeral: false,
+		});
 
-		const contentToSend = await request(interaction, config, website);
+		websites.forEach(async (website, key, arr) => {
+			const config = {
+				method: 'get',
+				url: `https://detector.scamsniffer.io/api/detect?link=${website}&force=0`,
+				headers: {
+					Accept: 'application/json',
+					'X-API-KEY': process.env.SCAMSNIFFER_API_KEY,
+				},
+			};
+
+			await request(interaction, config, website).then(async (res) => {
+				const resType = res.type;
+				const resContent = res.content;
+				const resEmbed = res.embed;
+				const resIsSafe = res.isSafe;
+
+				isSafe = resIsSafe;
+
+				const content = {
+					content: resContent,
+					embeds: [resEmbed],
+					ephemeral: false,
+				};
+
+				if (!isSafe) {
+					// As soon as isSafe is false reply and finish
+					if (resType === 'reply') {
+						await interaction.reply(content);
+					} else if (resType === 'editReply') {
+						await interaction.editReply(content);
+					}
+					return; // Finish
+				} else {
+					if (Object.is(arr.length - 1, key)) {
+						// Only send on the last element
+						if (resType === 'reply') {
+							await interaction.reply(content);
+						} else if (resType === 'editReply') {
+							await interaction.editReply(content);
+						}
+					}
+				}
+			});
+		});
 	},
 };
